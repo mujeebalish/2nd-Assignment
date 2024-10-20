cd297693f415a1b6dd0fe71fb82a8b5393e17f82
mujeebalish <mujeebrehman12122@gmail.com>
2024-10-20 04:03:42 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,94 @@
+// Arithmetic Operations
+/*document.write("<u>" + "<h3>" + "Result" + "</h3>" + "</u>" + "<br>");
+var num1 = 100;
+document.write("<u>" + "The value of num1 is:" + " " + num1 + "</u>" + "<br>" + "<br>");
+++num1;
+document.write("The value of ++num1 is:" + " " + 101 + "<br>");
+document.write("After pre increment. now The value of ++num1 is:" + " " + 101 + "<br>" + "<br>");
+num1++;
+document.write("The value of num1++ is:" + " " + 101 + "<br>");
+document.write("After post increment. now The value of num1++ is:" + " " + 102 + "<br>" + "<br>");
+--num1;
+document.write("The value of --num1 is:" + " " + 101 + "<br>");
+document.write("After pre decrement. now The value of --num1 is:" + " " + 101 + "<br>" + "<br>");
+num1--;
+document.write("The value of num1-- is:" + " " + 101 + "<br>");
+document.write("After post decrement. now The value of num1-- is:" + " " + 100 + "<br>" + "<br>");*/
+/*var userGreet = prompt("Enter you name:");
+var welcome = 
+document.write("welcome to AlishWDev World" + " " + userGreet);*/
+                // Multiplication Table
+/*document.write("<h3>"+"Multiplication Table"+"<h3>"+"<br>");
+var userInpNmbr = +prompt("Enter your value",);
+var mulT1 = userInpNmbr*1;
+var mulT2 = userInpNmbr*2;
+var mulT3 = userInpNmbr*3;
+var mulT4 = userInpNmbr*4;
+var mulT5 = userInpNmbr*5;
+var mulT6 = userInpNmbr*6;
+var mulT7 = userInpNmbr*7;
+var mulT8 = userInpNmbr*8;
+var mulT9 = userInpNmbr*9;
+var mulT10 = userInpNmbr*10;
+var userNumber = userInpNmbr;
+document.write(userNumber + "x" + "1"+"="+ mulT1 +"<br>"+userNumber + "x" + "2"+"="+ mulT2 +"<br>"+userNumber + "x" + "3"+"="+ mulT3 +"<br>" + userNumber + "x" + "4"+"="+ mulT4 +"<br>"+userNumber + "x" + "5"+"="+ mulT5 +"<br>"+userNumber + "x" + "6"+"="+ mulT6 +"<br>"+userNumber + "x" + "7"+"="+ mulT7 +"<br>"+userNumber + "x" + "8"+"="+ mulT8 +"<br>"+userNumber + "x" + "9"+"="+ mulT9 +"<br>"+userNumber + "x" + "10"+"="+ mulT10 +"<br>"
+);*/
+
+/*//Default Response.
+var defultResponse = 
+var mulT1 = userInpNmbr*1;
+var mulT2 = userInpNmbr*2;
+var mulT3 = userInpNmbr*3;
+var mulT4 = userInpNmbr*4;
+var mulT5 = userInpNmbr*5;
+var mulT6 = userInpNmbr*6;
+var mulT7 = userInpNmbr*7;
+var mulT8 = userInpNmbr*8;
+var mulT9 = userInpNmbr*9;
+var mulT10 = userInpNmbr*10;
+var userNumber = userInpNmbr;
+document.write(userNumber + "x" + "1"+"="+ mulT1 +"<br>"+userNumber + "x" + "2"+"="+ mulT2 +"<br>"+userNumber + "x" + "3"+"="+ mulT3 +"<br>" + userNumber + "x" + "4"+"="+ mulT4 +"<br>"+userNumber + "x" + "5"+"="+ mulT5 +"<br>"+userNumber + "x" + "6"+"="+ mulT6 +"<br>"+userNumber + "x" + "7"+"="+ mulT7 +"<br>"+userNumber + "x" + "8"+"="+ mulT8 +"<br>"+userNumber + "x" + "9"+"="+ mulT9 +"<br>"+userNumber + "x" + "10"+"="+ mulT10 +"<br>"
+); */
+                         // Mark Shhet Table.
+/*var english = +prompt("Enter your English Marks");
+var math = +prompt("Enter your Math  Marks");
+var urdu = +prompt("Enter your Urdu Marks");
+var englishMarks = english;
+var mathMarks = math;
+var urduMarks = urdu;
+var englishTotal = 100;
+var mathTotal = 100;
+var urduTotal = 100;
+var totalMarks = englishTotal+mathTotal+urduTotal;
+var englishPrcent = englishMarks/englishTotal*100;
+document.write("<table border='1px'> "+"<tr>"+"<th>"+"Subject" +"</th>"+ "<th>"+"Total Marks" +"</th>"+"<th>"+"Obtaind Marks" +"</th>"+"<th>"+"Percentage" +"</th>"+"</tr>"+ "<br> "+"<tr>"+"<td>"+"English"+ " "+englishTotal+ " "+englishMarks+ " "+englishPrcent + "%" +"</td>"+"</tr>"+"</table>")*/
+
+// USER INPIUT AND CONDITIONAL STATEMENT
+/*var cityName = prompt("Enter City Name");
+if(cityName == "karachi"){
+    alert('"Welcome to the city of lights"');
+    }*/
+    /*var gender = prompt("Enter Your Gender");
+    if(gender == "male"){
+        alert("Good morning" + " " + "Sir")
+    }if(gender == "female"){
+        alert("Good morning" + " " + "Ma'am")
+    }*/
+   
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
